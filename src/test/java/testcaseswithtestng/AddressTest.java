2797954e6d3a9538bfbc5765d9c3d99f71bbcd66
CrioUser <crio-user@criodo.com>
2022-06-10 11:56:45 +0530

    [build.gradle] Add error stack trace and message

--- src/test/java/testcaseswithtestng/AddressTest.java
+++ src/test/java/testcaseswithtestng/AddressTest.java
@@ -2,6 +2,7 @@ package testcaseswithtestng;
 
 import pages.AddressPage;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AddressTest extends BaseTest{
@@ -9,6 +10,8 @@ public class AddressTest extends BaseTest{
 	public void verifyAddingAddress() {
 		AddressPage addressPage = new AddressPage();
 		addressPage.addAddress("Pune");
+
+		Assert.assertEquals("Crio.Do: Project-based learning platform for developer", "Crio.Do: Project-based learning platform for developers", "Titles of the website do not match");
 	}
 	
 }
